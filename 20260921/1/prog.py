19497ada6563feac44c3d9c98d6f7a472e4ee7a3
Лысак Владимир <s02240197@vm-mz3-12.prac.cs.msu.ru>
2026-09-21 17:05:13 +0300

    HW Task 1

--- 20260921/1/prog.py
+++ 20260921/1/prog.py
@@ -1,1 +1,13 @@
-print("123")
+a = int(input())
+if not a % 50:
+	print("A + ", end="")
+else: 
+	print("A - ", end="")
+if not a % 25 and a % 2:
+	print("B + ", end="")
+else: 
+	print("B - ", end="")
+if not a % 8:
+	print("C + ", end="")
+else: 
+	print("C - ")
